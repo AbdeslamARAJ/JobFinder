6e4fcab43238b3a18384e4b4139e80e964ffc224
Abdeslam ARAJ <abdeslam.araj@gmail.com>
2021-03-18 16:37:45 +0100

    Init Project
    Init Project

---
+++ JobFinder/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using JobFinder.Model;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace JobFinder.Controllers
+{
+    public class HomeController : Controller
+    {
+        private string BASE_URL = "https://jobs.github.com/positions.json";
+        public async Task<IActionResult> Index()
+        {
+            string url = BASE_URL + "?search=trend";
+            return View(await GetJobs(url));
+        }
+
+        [Route("search")]
+        public async Task<IActionResult> Search()
+        {
+            string position = HttpContext.Request.Query["position"].ToString();
+            string location = HttpContext.Request.Query["location"].ToString();
+
+            string url = BASE_URL;
+
+            if (location.Length > 0)
+            {
+                url += "?location=" + location;
+            }
+            if (position.Length > 0)
+            {
+                url += "?search=" + position;
+            }
+            return View(await GetJobs(url));
+        }
+
+        private async Task<List<Job>> GetJobs(string url)
+        {
+            List<Job> jobs = new List<Job>();
+
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync(url))
+                {
+                    string result = await response.Content.ReadAsStringAsync();
+                    jobs = JsonConvert.DeserializeObject<List<Job>>(result);
+                }
+            }
+            return jobs;
+        }
+    }
+}
---
+++ JobFinder/Model/Job.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JobFinder.Model
+{
+    public class Job
+    {
+        public string Id { get; set; }
+        public string Type { get; set; }
+        public string Url { get; set; }
+        public string Created_at { get; set; }
+        public string Company { get; set; }
+        public string Company_url { get; set; }
+        public string Location { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string How_to_apply { get; set; }
+        public string Company_logo { get; set; }
+    }
+}
